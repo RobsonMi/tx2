34c264babc5c297f837cb0c2a4cd070827eeccc6
Robert Miroszewski <r.miroszewski@gmail.com>
2023-10-30 21:48:19 +0100

    Fixes to errors in code

--- src/utils/probes/EWMA.js
+++ src/utils/probes/EWMA.js
@@ -1,29 +1,28 @@
-
-var units = require('./units');
+var units = require("./units");
 
 module.exports = ExponentiallyWeightedMovingAverage;
 
 function ExponentiallyWeightedMovingAverage(timePeriod, tickInterval) {
-  this._timePeriod   = timePeriod || 1 * units.MINUTE;
-  this._tickInterval = tickInterval || ExponentiallyWeightedMovingAverage.TICK_INTERVAL;
-  this._alpha        = 1 - Math.exp(-this._tickInterval / this._timePeriod);
-  this._count        = 0;
-  this._rate         = 0;
-};
+  this._timePeriod = timePeriod * units.SECONDS || 1 * units.MINUTES;
+  this._tickInterval = tickInterval * units.SECONDS || ExponentiallyWeightedMovingAverage.TICK_INTERVAL;
+  this._alpha = 1 - Math.exp(-this._tickInterval / this._timePeriod);
+  this._count = 0;
+  this._rate = 0;
+}
 
 ExponentiallyWeightedMovingAverage.TICK_INTERVAL = 5 * units.SECONDS;
 
-ExponentiallyWeightedMovingAverage.prototype.update = function(n) {
+ExponentiallyWeightedMovingAverage.prototype.update = function (n) {
   this._count += n;
 };
 
-ExponentiallyWeightedMovingAverage.prototype.tick = function() {
+ExponentiallyWeightedMovingAverage.prototype.tick = function () {
   var instantRate = this._count / this._tickInterval;
-  this._count     = 0;
+  this._count = 0;
 
-  this._rate += (this._alpha * (instantRate - this._rate));
+  this._rate += this._alpha * (instantRate - this._rate);
 };
 
-ExponentiallyWeightedMovingAverage.prototype.rate = function(timeUnit) {
+ExponentiallyWeightedMovingAverage.prototype.rate = function (timeUnit) {
   return (this._rate || 0) * timeUnit;
 };
--- src/utils/probes/Meter.js
+++ src/utils/probes/Meter.js
@@ -1,33 +1,32 @@
-
-var units = require('./units')
-var EWMA  = require('./EWMA')
+var units = require("./units");
+var EWMA = require("./EWMA");
 
 function Meter(opts) {
-  var self = this
+  var self = this;
 
-  this._tickInterval = units.SECONDS
-  this._samples = opts.seconds || 1
-  this._timeframe = opts.timeframe || 60
+  // this._tickInterval = units.SECONDS
+  this._tickInterval = opts.seconds || 1;
+  this._timeframe = opts.timeframe || 60;
 
-  this._rate     = new EWMA(units.SECONDS, this._tickInterval)
+  this._rate = new EWMA(this._timeframe, this._tickInterval);
 
-  this._interval = setInterval(function() {
-    self._rate.tick()
-  }, this._tickInterval)
+  this._interval = setInterval(function () {
+    self._rate.tick();
+  }, this._tickInterval);
 
-  this._interval.unref()
+  this._interval.unref();
 }
 
-Meter.RATE_UNIT     = units.SECONDS
+Meter.RATE_UNIT = units.SECONDS;
 
-Meter.prototype.mark = function(n) {
-  n = n || 1
+Meter.prototype.mark = function (n) {
+  n = n || 1;
 
-  this._rate.update(n)
-}
+  this._rate.update(n);
+};
 
-Meter.prototype.val = function() {
-  return Math.round(this._rate.rate(Meter.RATE_UNIT) * 100 ) / 100
-}
+Meter.prototype.val = function () {
+  return Math.round(this._rate.rate(Meter.RATE_UNIT) * 100) / 100;
+};
 
-module.exports = Meter
+module.exports = Meter;
